71d01c485886958f398f39bee37e9dc7c6164f32
SoapyMan <ishurumov@gmail.com>
2024-09-13 22:09:17 +0500

    Editor: Refactor and fix LightmapCompiler crashes

--- SourceCode/Editor/LightmapCompiler/RasterTable.h
+++ SourceCode/Editor/LightmapCompiler/RasterTable.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <assert.h>										// assert()
 #include "SimpleTriangleRasterizer.h"					// CSimpleTriangleRasterizer
 
 // This class is to speed up raycasting queries.
@@ -13,171 +12,166 @@ class CRasterTable
 {
 public:
 	//! constructor
-	CRasterTable( void );
+	CRasterTable(void);
 
 	//! destructor
-	virtual ~CRasterTable( void );
+	virtual ~CRasterTable(void);
 
 	//! alloc raster, after that Phase 1 has started
 	//! /param indwXSize 1..
 	//! /param indwYSize 1..
 	//! /return true=success, false otherwise
-	bool Init( const DWORD indwXSize, const DWORD indwYSize );
+	bool Init(const DWORD indwXSize, const DWORD indwYSize);
 
 	//!
 	//! free data
-	void DeInit( void );
+	void DeInit(void);
 
 	//! Has to be called in Phase 1, after Phase 2 has started
 	//! /return true=success, false otherwise
-	bool PreProcess( void );
+	bool PreProcess(void);
 
 	//! call this per triangle after Init and before PreProcess
 	//! after PreProcess call it again for every triangle
 	//! /param infX[3] x coordiantes of the 3 vertices in raster units
 	//! /param infX[3] y coordiantes of the 3 vertices in raster units
 	//! /param inElement element you want to store
-	void PutInTriangle( float infX[3], float infY[3], T &inElement );
+	void PutInTriangle(float infX[3], float infY[3], T& inElement);
 
 	//! returns pointer to zero terminated array of pointers
 	//! /param iniX 0..m_dwXSize-1
 	//! /param iniY 0..m_dwYSize-1
 	//! /return
-	T *GetElementsAt( int iniX, int iniY );
+	T* GetElementsAt(int iniX, int iniY);
 
 	//!
 	//! /return memory consumption in bytes O(1)
-	DWORD CalcMemoryConsumption( void );
+	DWORD CalcMemoryConsumption(void);
 
 	//!
 	//! /param inPathFileName filename with path and extension
-	void Debug( const char *inPathFileName ) const;
+	void Debug(const char* inPathFileName) const;
 
 	//!
 	//! /param outPitchInBytes
 	//! /return 
-	DWORD *GetDebugData( DWORD &outPitchInBytes ) const;
+	DWORD* GetDebugData(DWORD& outPitchInBytes) const;
 
 	//!
 	//! /return width in raster elements
-	DWORD GetWidth( void );
+	DWORD GetWidth(void);
 
 	//!
 	//! /return height in raster elements
-	DWORD GetHeight( void );
+	DWORD GetHeight(void);
 
 	//! to save memory and make it faster - less cache misses)
 	// IF YOU WANNA USE THIS FUNCTION, CLEAR IT FIRST BECAUSE IT SIMPLY DOES NOT WORK
-	void Compress( void );
+	void Compress(void);
 
 	// **************************************************************
 
 	union
 	{
-		DWORD	*						m_pDataCounter;			//!< used in Phase 1 [m_dwXSize*m_dwYSize]
-		T **							m_pDataPtr;				//!< used in Phase 2 [m_dwXSize*m_dwYSize]
+		DWORD_PTR*	m_pDataCounter;		//!< used in Phase 1 [m_dwXSize*m_dwYSize]
+		T**			m_pDataPtr;			//!< used in Phase 2 [m_dwXSize*m_dwYSize]
 	};
 
 private:
-	T *									m_pExtraMemoryPool;		//!< for the pointers to store (zero terminated)  (sorted in ascending order) [m_ExtraMemorySize]
-	DWORD								m_ExtraMemorySize;		//!< extra memroy pool size in StoredElementPtr elements
+	T*			m_pExtraMemoryPool;		//!< for the pointers to store (zero terminated)  (sorted in ascending order) [m_ExtraMemorySize]
+	DWORD		m_ExtraMemorySize;		//!< extra memroy pool size in StoredElementPtr elements
 
-	DWORD								m_dwXSize;				//!< width of the buffer 1.. if allocated, otherwise 0
-	DWORD								m_dwYSize;				//!< height of the buffer 1.. if allocated, otherwise 0
-
-
+	DWORD		m_dwXSize;				//!< width of the buffer 1.. if allocated, otherwise 0
+	DWORD		m_dwYSize;				//!< height of the buffer 1.. if allocated, otherwise 0
+	DWORD		m_DataSize;
 
 	//! callback function
-	class CPixelIncrement: public CSimpleTriangleRasterizer::IRasterizeSink
+	class CPixelIncrement : public CSimpleTriangleRasterizer::IRasterizeSink
 	{
 	public:
 
 		//! constructor
-		CPixelIncrement( DWORD *inpBuffer, DWORD indwWidth, DWORD indwHeight )
+		CPixelIncrement(DWORD_PTR* inpBuffer, DWORD indwWidth, DWORD indwHeight)
 		{
-			m_pBuffer=inpBuffer;
-			m_dwWidth=indwWidth;
-			m_dwHeight=indwHeight;
+			m_pBuffer = inpBuffer;
+			m_dwWidth = indwWidth;
+			m_dwHeight = indwHeight;
 		}
 
 		//!
-		virtual void Line( const float infXLeft, const float infXRight,
-						const int iniLeft, const int iniRight, const int iniY )
+		virtual void Line(const float infXLeft, const float infXRight,
+			const int iniLeft, const int iniRight, const int iniY)
 		{
-			assert(iniLeft>=0);
-			assert(iniY>=0);
-			assert(iniRight<=(int)m_dwWidth);
-			assert(iniY<(int)m_dwHeight);
+			CRYASSERT(iniLeft >= 0);
+			CRYASSERT(iniY >= 0);
+			CRYASSERT(iniRight <= (int)m_dwWidth);
+			CRYASSERT(iniY < (int)m_dwHeight);
 
-			DWORD *pMem=&m_pBuffer[iniLeft+iniY*m_dwWidth];
+			DWORD_PTR* pMem = &m_pBuffer[iniLeft + iniY * m_dwWidth];
 
-			for(int x=iniLeft;x<iniRight;x++)
+			for (int x = iniLeft; x < iniRight; x++)
 			{
-				*pMem=*pMem+1;
+				*pMem = *pMem + 1;
 				pMem++;
 			}
 		}
 
-		DWORD *			m_pBuffer;				//!<
-		DWORD			m_dwWidth;				//!< x= [0,m_dwWidth[
-		DWORD			m_dwHeight;				//!< y= [0,m_dwHeight[
+		DWORD_PTR*	m_pBuffer;				//!<
+		DWORD		m_dwWidth;				//!< x= [0,m_dwWidth[
+		DWORD		m_dwHeight;				//!< y= [0,m_dwHeight[
 	};
 
 
 	//! callback function
 	template <class T>
-	class CPixelAddArrayElement: public CSimpleTriangleRasterizer::IRasterizeSink
+	class CPixelAddArrayElement : public CSimpleTriangleRasterizer::IRasterizeSink
 	{
 	public:
-
 		//! constructor
-		CPixelAddArrayElement( T **inpBuffer, DWORD indwWidth, T *inElementPtr )
+		CPixelAddArrayElement(T** inpBuffer, DWORD indwWidth, DWORD indwHeight, T& inElementPtr)
+			: m_pBuffer(inpBuffer)
+			, m_dwWidth(indwWidth)
+			, m_dwHeight(indwHeight)
+			, m_ElementPtr(inElementPtr)
 		{
-			m_pBuffer=inpBuffer;
-			m_dwWidth=indwWidth;
-			m_ElementPtr=inElementPtr;
 		}
 
 		//!
-		__forceinline void ReturnPixel( T * &rPtr )
+		virtual void Line(const float infXLeft, const float infXRight, const int iniLeft, const int iniRight, const int iniY)
 		{
-			--rPtr;
-			*rPtr=*m_ElementPtr;
+			const int rowStart = iniY * m_dwWidth;
+
+			CRYASSERT_MSG(rowStart < m_dwWidth * m_dwHeight, "pos out of bounds %d, size %d", iniLeft, m_dwWidth * m_dwHeight);
+			CRYASSERT_MSG(iniLeft >= 0 && iniLeft < m_dwWidth, "iniLeft out of bounds %d, width %d", iniLeft, m_dwWidth);
+			CRYASSERT_MSG(iniRight >= 0 && iniRight < m_dwWidth, "iniRight out of bounds %d, width %d", iniLeft, m_dwWidth);
+			CRYASSERT_MSG(iniY >= 0 && iniY < m_dwHeight, "iniY out of bounds %d, height %d", iniLeft, m_dwHeight);
+
+			for (int x = iniLeft; x < iniRight; ++x)
+			{
+				--m_pBuffer[rowStart + x];
+				*m_pBuffer[rowStart + x] = m_ElementPtr;
+			}
 		}
 
-		//!
-		virtual void Line( const float infXLeft, const float infXRight,
-						const int iniLeft, const int iniRight, const int iniY )
-		{
-			T **pMem=&m_pBuffer[iniLeft+iniY*m_dwWidth];
-
-			for(int x=iniLeft;x<iniRight;x++)
-				ReturnPixel(*pMem++);
-		}
-
-		T *				m_ElementPtr;			//!< element to store
-		T **			m_pBuffer;				//!< pointer to the array buffer
-		DWORD			m_dwWidth;				//!< width of the buffer
+		T&		m_ElementPtr;	//!< element to store
+		T**		m_pBuffer;		//!< pointer to the array buffer
+		DWORD	m_dwWidth;		//!< width of the buffer
+		DWORD	m_dwHeight;
 	};
-
 };
-
-
 
 // constructor
 template <class T>
-CRasterTable<T>::CRasterTable( void )
+CRasterTable<T>::CRasterTable(void)
 {
-	assert(sizeof(DWORD)==sizeof(DWORD *));		// only for 32 Bit compiler
-
-	m_pDataPtr=0;
-	m_pExtraMemoryPool=0;
-	m_ExtraMemorySize=0;
+	m_pDataPtr = 0;
+	m_pExtraMemoryPool = 0;
+	m_ExtraMemorySize = 0;
 }
 
 // destructor
 template <class T>
-CRasterTable<T>::~CRasterTable( void )
+CRasterTable<T>::~CRasterTable(void)
 {
 	DeInit();
 }
@@ -186,277 +180,264 @@ CRasterTable<T>::~CRasterTable( void )
 
 // free data
 template <class T>
-void CRasterTable<T>::DeInit( void )
+void CRasterTable<T>::DeInit(void)
 {
-	delete [] m_pDataCounter;m_pDataCounter=0;
-	delete [] m_pExtraMemoryPool;m_pExtraMemoryPool=0;
-	m_ExtraMemorySize=0;
-	m_dwXSize=0;
-	m_dwYSize=0;
+	SAFE_DELETE_ARRAY(m_pDataCounter);
+	SAFE_DELETE_ARRAY(m_pExtraMemoryPool);
+	m_ExtraMemorySize = 0;
+	m_dwXSize = 0;
+	m_dwYSize = 0;
 }
 
 
 // alloc raster, after that Phase 1 has started
 template <class T>
-bool CRasterTable<T>::Init( const DWORD indwXSize, const DWORD indwYSize )
+bool CRasterTable<T>::Init(const DWORD indwXSize, const DWORD indwYSize)
 {
-	assert(!m_pDataCounter);
-	assert(!m_pExtraMemoryPool);	DeInit();
-	assert(indwXSize);
-	assert(indwYSize);
+	CRYASSERT(!m_pDataCounter);
+	CRYASSERT(!m_pExtraMemoryPool);
 
-	m_dwXSize=indwXSize;
-	m_dwYSize=indwYSize;
+	DeInit();
+	CRYASSERT(indwXSize);
+	CRYASSERT(indwYSize);
 
-	assert(sizeof(DWORD)==sizeof(T *));
-	m_pDataCounter=new DWORD[m_dwXSize*m_dwYSize];
+	m_dwXSize = indwXSize;
+	m_dwYSize = indwYSize;
+	m_DataSize = m_dwXSize * m_dwYSize;
 
-	if(m_pDataCounter)
-		memset(m_pDataCounter,0,m_dwXSize*m_dwYSize*sizeof(DWORD));
+	m_pDataCounter = new DWORD_PTR[m_DataSize];
+	memset(m_pDataCounter, 0, m_DataSize * sizeof(m_pDataCounter[0]));
 
-	return(m_pDataPtr!=0);
+	return(m_pDataPtr != 0);
 }
 
 
 // Has to be called in Phase1, after that Phase 2 has started
 template <class T>
-bool CRasterTable<T>::PreProcess( void )
+bool CRasterTable<T>::PreProcess(void)
 {
-	assert(m_pDataCounter);
-	assert(!m_pExtraMemoryPool);
-	assert(!m_ExtraMemorySize);
+	CRYASSERT(m_pDataCounter);
+	CRYASSERT(!m_pExtraMemoryPool);
+	CRYASSERT(!m_ExtraMemorySize);
 
-	DWORD dwSum=0;			// extra memroy pool size in StoredElementPtr elements
-
+	// extra memroy pool size in StoredElementPtr elements
+	DWORD dwSum = 0;
+	for (DWORD i = 0; i < m_DataSize; i++)
 	{
-		DWORD *ptr=m_pDataCounter;
-
-		for(DWORD i=0;i<m_dwXSize*m_dwYSize;i++)
-		{
-			DWORD dwNumEl=*ptr++;
-
-			if(dwNumEl!=0)
-				dwSum+=dwNumEl+1;		// Elements + NullTermination
-		}
+		DWORD_PTR dwNumEl = m_pDataCounter[i];
+		if (dwNumEl != 0)
+			dwSum += dwNumEl + 1; // Elements + NullTermination
 	}
 
-	if(dwSum==0)return(true);			// no information to store - no problem
+	if (dwSum == 0)
+		return true; // no information to store - no problem
 
-	m_pExtraMemoryPool=new T[dwSum];
-
-	m_ExtraMemorySize=dwSum;
-
-	if(!m_pExtraMemoryPool)return(false);
-
-	memset(m_pExtraMemoryPool,0,dwSum*sizeof(T));
+	m_pExtraMemoryPool = new T[dwSum];
+	m_ExtraMemorySize = dwSum;
+	memset(m_pExtraMemoryPool, 0, dwSum * sizeof(m_pExtraMemoryPool[0]));
 
 	// build the access structure for compressing the elements in a vector (pointer is in the beginning on the last element)
 	// after filing in the triangles once again the pointer is on the first element
+	T** pData = m_pDataPtr;
+	T* dest = m_pExtraMemoryPool;
+
+	for (DWORD i = 0; i < m_DataSize; i++)
 	{
-		DWORD *ptr1=m_pDataCounter;
-		T **ptr2=m_pDataPtr;
-		T *dest=m_pExtraMemoryPool;
-
-		for(DWORD i=0;i<m_dwXSize*m_dwYSize;i++)
+		DWORD_PTR dwNumEl = m_pDataCounter[i];
+		if (dwNumEl != 0)
 		{
-			DWORD dwNumEl=*ptr1++;
-
-			if(dwNumEl!=0)
-			{
-				dest+=dwNumEl;	
-				*ptr2++=dest;
-				dest++;							// Elements + NullTermination
-			}
-			else *ptr2++=0;
+			dest += dwNumEl;
+			*pData++ = dest;
+			dest++;	// Elements + NullTermination
 		}
+		else
+			*pData++ = 0;
 	}
 
-	return(true);
+	return true;
 }
-
-
-
-
-
-
 
 
 // call this per triangle after Init and before PreProcess
 // after PreProcess call it again for every triangle
 template <class T>
-void CRasterTable<T>::PutInTriangle( float infX[3], float infY[3], T &inElement )
+void CRasterTable<T>::PutInTriangle(float infX[3], float infY[3], T& inElement)
 {
-	float fU[3],fV[3];
-
-	for(int i=0;i<3;i++)
+	float fU[3], fV[3];
+	for (int i = 0; i < 3; i++)
 	{
-		fU[i]=infX[i];fV[i]=infY[i];
+		fU[i] = infX[i]; 
+		fV[i] = infY[i];
 	}
 
-	CSimpleTriangleRasterizer Rasterizer(m_dwXSize,m_dwYSize);
-
-	if(m_pExtraMemoryPool==0)			// Phase 1 
+	CSimpleTriangleRasterizer Rasterizer(m_dwXSize, m_dwYSize);
+	if (!m_pExtraMemoryPool)
 	{
-		CPixelIncrement pix(m_pDataCounter,m_dwXSize,m_dwYSize);
-
-		Rasterizer.CallbackFillConservative(fU,fV,&pix);
+		// Phase 1 
+		CPixelIncrement pix(m_pDataCounter, m_dwXSize, m_dwYSize);
+		Rasterizer.CallbackFillConservative(fU, fV, &pix);
 	}
-	else													// Phase 2
+	else
 	{
-		CPixelAddArrayElement<T> pix(m_pDataPtr,m_dwXSize,&inElement);
-
-		Rasterizer.CallbackFillConservative(fU,fV,&pix);
+		// Phase 2
+		CPixelAddArrayElement<T> pix(m_pDataPtr, m_dwXSize, m_dwYSize, inElement);
+		Rasterizer.CallbackFillConservative(fU, fV, &pix);
 	}
 }
 
-
-
 // returns pointer to zero terminated array of pointers
 template <class T>
-T *CRasterTable<T>::GetElementsAt( int iniX, int iniY )
+T* CRasterTable<T>::GetElementsAt(int iniX, int iniY)
 {
-	assert(iniX>=0);
-	assert(iniX<(int)m_dwXSize);
-	assert(iniY>=0);
-	assert(iniY<(int)m_dwYSize);
+	CRYASSERT(iniX >= 0);
+	CRYASSERT(iniX < (int)m_dwXSize);
+	CRYASSERT(iniY >= 0);
+	CRYASSERT(iniY < (int)m_dwYSize);
 
-	T *pRet=m_pDataPtr[iniY*m_dwXSize+iniX];
-
-	if(pRet)
-		assert(*pRet);			// no pointer in the raster should point to empty list
+	T* pRet = m_pDataPtr[iniY * m_dwXSize + iniX];
+	if (pRet)
+	{
+		CRYASSERT_MSG(*pRet, "no pointer in the raster should point to empty list");
+	}
 
 	return(pRet);
 }
 
 
- 
-
 // returns pointer to zero terminated array of pointers
 // IF YOU WANNA USE THIS FUNCTION, CLEAR IT FIRST BECAUSE IT SIMPLY DOES NOT WORK
 template <class T>
-void CRasterTable<T>::Compress( void )
+void CRasterTable<T>::Compress(void)
 {
-	if(!m_pExtraMemoryPool)return;
+	if (!m_pExtraMemoryPool)
+		return;
 
-	T *pDestStart=0;
-	DWORD dwNewMemorySize=0;
+	T* pDestStart = 0;
+	DWORD dwNewMemorySize = 0;
 
-	for(DWORD i=0;i<m_dwXSize*m_dwYSize;i++)
+	for (DWORD i = 0; i < m_DataSize; i++)
 	{
-		T *pSrcStart=m_pDataPtr[i];	// pointer from the raster element
+		T* pSrcStart = m_pDataPtr[i];	// pointer from the raster element
 
-		if(!pSrcStart)continue;			// no elements there
+		if (!pSrcStart)
+			continue;			// no elements there
 
+		bool bAddBucket = true;
+		T* pSrc = pSrcStart, * pDest = pDestStart;
+
+		if (pDestStart)
 		{
-			bool bAddBucket=true;
-			T *pSrc=pSrcStart, *pDest=pDestStart;
-
-			if(pDestStart)
-			while(*pSrc == *pDest)
+			while (*pSrc == *pDest)
 			{
-				if(*pSrc==0)
+				if (*pSrc == 0)
 				{
 					// buckets are fully identical
-					bAddBucket=false;
+					bAddBucket = false;
 					break;
 				}
 
-				pDest++;pSrc++;
+				pDest++; pSrc++;
 			}
-
-			if(bAddBucket)
-			{
-				if(!pDestStart) pDestStart=m_pExtraMemoryPool;
-				else
-				{
-					while(*pDestStart)pDestStart++;	pDestStart++;
-				}
-			
-				// copy bucket
-				T *pSrc=pSrcStart, *pDest=pDestStart;
-
-				while(*pSrc)
-					{ *pDest++=*pSrc++;dwNewMemorySize++; }
-
-				*pDest++=0;										// 0 terminate
-				assert(*pDestStart);					// no pointer in the raster should point to empty list
-
-				m_pDataPtr[i]=pDestStart;
-			}
-			else m_pDataPtr[i]=pDestStart;
 		}
 
+		if (bAddBucket)
+		{
+			if (!pDestStart)
+			{
+				pDestStart = m_pExtraMemoryPool;
+			}
+			else
+			{
+				while (*pDestStart)
+					pDestStart++;
+				pDestStart++;
+			}
+
+			// copy bucket
+			T* pSrc = pSrcStart;
+			*pDest = pDestStart;
+
+			while (*pSrc)
+			{
+				*pDest++ = *pSrc++; 
+				dwNewMemorySize++;
+			}
+
+			*pDest++ = 0;										// 0 terminate
+			CRYASSERT(*pDestStart);					// no pointer in the raster should point to empty list
+
+			m_pDataPtr[i] = pDestStart;
+		}
+		else
+			m_pDataPtr[i] = pDestStart;
 	}
 
-	T *newMem=new T[dwNewMemorySize];
+	if (!dwNewMemorySize)
+		return;
 
-	if(!newMem)return;		// no memory - no compression
+	T* newMem = new T[dwNewMemorySize];
+	if (!newMem)
+		return;
 
 	// copy in new buffer
-	memcpy(newMem,m_pExtraMemoryPool,dwNewMemorySize*sizeof(T));
+	memcpy(newMem, m_pExtraMemoryPool, dwNewMemorySize * sizeof(T));
 
 	// correct the pointers to the new buffer
-	for(i=0;i<m_dwXSize*m_dwYSize;i++)
+	for (i = 0; i < m_DataSize; i++)
 	{
-		if(m_pDataPtr[i])
-		{
-			assert(*m_pDataPtr[i]);					// no pointer in the raster should point to empty list
-			m_pDataPtr[i]=(T *)( (DWORD)(m_pDataPtr[i]) - (DWORD)m_pExtraMemoryPool + (DWORD)newMem);
-			assert(*m_pDataPtr[i]);					// no pointer in the raster should point to empty list
-		}
+		if (!m_pDataPtr[i])
+			continue;
+
+		CRYASSERT_MSG(*m_pDataPtr[i], "no pointer in the raster should point to empty list");
+		m_pDataPtr[i] = (T*)((DWORD_PTR)(m_pDataPtr[i]) - (DWORD_PTR)m_pExtraMemoryPool + (DWORD_PTR)newMem);
+		CRYASSERT_MSG(*m_pDataPtr[i], "no pointer in the raster should point to empty list");
 	}
 
-	delete [] m_pExtraMemoryPool;
-
 	// set new buffer
-	m_pExtraMemoryPool=newMem;
+	delete[] m_pExtraMemoryPool;
+	m_pExtraMemoryPool = newMem;
 
-	float fRatio=(float)dwNewMemorySize/(float)m_ExtraMemorySize;
+	float fRatio = (float)dwNewMemorySize / (float)m_ExtraMemorySize;
 }
 
 
 // calculates memory consumption in bytes O(1)
 template <class T>
-DWORD CRasterTable<T>::CalcMemoryConsumption( void	)
+DWORD CRasterTable<T>::CalcMemoryConsumption(void)
 {
-	return(m_dwXSize*m_dwYSize*sizeof(m_pDataCounter)+m_ExtraMemorySize*sizeof(T));
+	return (m_DataSize * sizeof(m_pDataCounter[0]) + m_ExtraMemorySize * sizeof(T));
 }
 
 
 template <class T>
-void CRasterTable<T>::Debug( const char *inPathFileName ) const
+void CRasterTable<T>::Debug(const char* inPathFileName) const
 {
 #ifdef _TGA_LOADER_INCLUDED
-	for(DWORD i=0;i<m_dwXSize*m_dwYSize;i++)
-		m_pDataCounter[i]=m_pDataCounter[i]*10+0x0800;
+	for (DWORD i = 0; i < m_DataSize; i++)
+		m_pDataCounter[i] = m_pDataCounter[i] * 10 + 0x0800;
 
-	PIX_SaveTGA32(inPathFileName,(unsigned char *)m_pDataCounter,m_dwXSize,m_dwYSize,false,false);
+	PIX_SaveTGA32(inPathFileName, (unsigned char*)m_pDataCounter, m_dwXSize, m_dwYSize, false, false);
 
-	for(DWORD i=0;i<m_dwXSize*m_dwYSize;i++)
-		m_pDataCounter[i]=(m_pDataCounter[i]-0x0800)/10;
+	for (DWORD i = 0; i < m_DataSize; i++)
+		m_pDataCounter[i] = (m_pDataCounter[i] - 0x0800) / 10;
 #endif
 }
 
-
-
 template <class T>
-DWORD *CRasterTable<T>::GetDebugData( DWORD &outPitchInBytes ) const
+DWORD* CRasterTable<T>::GetDebugData(DWORD& outPitchInBytes) const
 {
-	outPitchInBytes=m_dwXSize*sizeof(DWORD);
-	return(m_pDataCounter);
-}
-
-
-
-template <class T>
-DWORD CRasterTable<T>::GetWidth( void )
-{
-	return(m_dwXSize);
+	outPitchInBytes = m_dwXSize * sizeof(DWORD_PTR);
+	return m_pDataCounter;
 }
 
 template <class T>
-DWORD CRasterTable<T>::GetHeight( void )
+DWORD CRasterTable<T>::GetWidth(void)
 {
-	return(m_dwYSize);
+	return m_dwXSize;
 }
 
+template <class T>
+DWORD CRasterTable<T>::GetHeight(void)
+{
+	return m_dwYSize;
+}
