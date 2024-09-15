c5077a00b2ef4dbbe1857c9a96d8e52ad4761d7d
SoapyMan <ishurumov@gmail.com>
2024-09-15 18:13:50 +0500

    XRenderD3D9: track every matrix change for SetClipPlane

--- SourceCode/RenderDll/XRenderD3D9/D3DCGVProgram.h
+++ SourceCode/RenderDll/XRenderD3D9/D3DCGVProgram.h
@@ -632,22 +632,13 @@ public:
 	{
 		CD3D9Renderer* r = gcpRendD3D;
 
-		switch (ParamBind->m_eCGParamType)
+		if (r->m_RP.m_ClipPlaneEnabled == 2)
 		{
-		case ECGP_Matr_ViewProj:
-		case ECGP_Matr_View_IT:
-		case ECGP_Matr_View_I:
-		case ECGP_Matr_View_T:
-		case ECGP_Matr_View:
-			if (r->m_RP.m_ClipPlaneEnabled == 2)
-			{
-				// Transform clip plane to clip space
-				Plane srcPlane(r->m_RP.m_CurClipPlane.m_Normal, r->m_RP.m_CurClipPlane.m_Dist);
-				Plane transformedPlane = TransformPlane2(r->m_InvCameraProjMatrix, srcPlane);
-				r->m_pd3dDevice->SetClipPlane(0, &transformedPlane.n[0]);
-				r->m_RP.m_ClipPlaneWasOverrided = 2;
-			}
-			break;
+			// Transform clip plane to clip space
+			Plane srcPlane(r->m_RP.m_CurClipPlane.m_Normal, r->m_RP.m_CurClipPlane.m_Dist);
+			Plane transformedPlane = TransformPlane2(r->m_InvCameraProjMatrix, srcPlane);
+			r->m_pd3dDevice->SetClipPlane(0, &transformedPlane.n[0]);
+			r->m_RP.m_ClipPlaneWasOverrided = 2;
 		}
 
 		LPDIRECT3DDEVICE9 dv = r->mfGetD3DDevice();
